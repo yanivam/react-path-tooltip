7ce2cb12e1aa77d684a98717b1178ce78381cae5
ez <ez@skippy.home>
2020-04-03 18:27:06 -0400

    change example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -8,8 +8,9 @@ function App() {
     < div className="App" >
       < div className="Main">
         <svg width="200px" height="200px">
-          <circle cx={50} cy={50} r={10} fill="red" />
-          <SVGTooltip tip="Hello world" state={true}/>
+          <SVGTooltip tip="Hello world" state={true}>
+            <circle cx={50} cy={50} r={10} fill="red" />
+          </SVGTooltip>
         </svg>
       </div>
     </div>
