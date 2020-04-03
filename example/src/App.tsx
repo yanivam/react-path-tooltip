9e0ddf51a5d6abce2bf757e4d99bc987f8a058aa
Yaniv Amiri <yanivam123@gmail.com>
2020-04-03 18:01:40 -0400

    Initial permanent tooltip

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,15 @@
 import React from "react"
 import "./App.css"
-import { SvgTooltip } from "react-tooltip-for-svg" // import the package
+import { SVGTooltip } from "./ReactTooltipForSVG" // import the package
 
 function App() {
-
-  const mySvg = 
 
   return (
     < div className="App" >
       < div className="Main">
         <svg width="200px" height="200px">
-          <circle cx={50} cy={50} r={10} fill="red">
-            <SvgToolTip tip="tooltip assigned to cricle" />
-          </circle>
+          <circle cx={50} cy={50} r={10} fill="red" />
+          <SVGTooltip tip="Hello world" state={true}/>
         </svg>
       </div>
     </div>
