6af3b98841be48768d70cc07eec5f6d8f1a3a62b
ezborgy <49008885+ezborgy@users.noreply.github.com>
2020-04-19 13:09:02 -0400

    Update App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,7 +5,7 @@ import { PathTooltip } from "./ReactPathTooltip" // import the package
 function App() {
 
   const svgRef=React.createRef<SVGSVGElement>()
-  const circleRef=React.createRef<SVGCircleElement>()
+  const circleRedRef=React.createRef<SVGCircleElement>()
   const circleGreenRef=React.createRef<SVGCircleElement>()
   const circleBlueRef=React.createRef<SVGCircleElement>()
 
@@ -13,15 +13,16 @@ function App() {
     < div className="App" >
       < div className="Main">
         <svg width="200px" height="400px" ref={svgRef}>
-          <PathTooltip svgRef={svgRef} pathRef={circleRef} tip="Hello world">
-            <circle cx={50} cy={50} r={10} fill="red" ref={circleRef} />
-          </PathTooltip>
-          <PathTooltip svgRef={svgRef} pathRef={circleGreenRef} tip="Hmmmmmmm">
-            <circle cx={150} cy={200} r={20} fill="green" ref={circleGreenRef} />
-          </PathTooltip>
-          <PathTooltip svgRef={svgRef} pathRef={circleBlueRef} tip="HW">
-            <circle cx={101} cy={101} r={30} fill="blue" ref={circleBlueRef} />
-          </PathTooltip>
+          
+          <circle cx={50} cy={50} r={10} fill="red" ref={circleRedRef} />
+          <PathTooltip svgRef={svgRef} pathRef={circleRef} tip="Hello world" />
+          
+          <circle cx={150} cy={200} r={20} fill="green" ref={circleGreenRef} />
+          <PathTooltip svgRef={svgRef} pathRef={circleGreenRef} tip="Hmmmmmmm" />
+          
+          <circle cx={101} cy={101} r={30} fill="blue" ref={circleBlueRef} />
+          <PathTooltip svgRef={svgRef} pathRef={circleBlueRef} tip="HW" />
+
         </svg>
       </div>
     </div>
